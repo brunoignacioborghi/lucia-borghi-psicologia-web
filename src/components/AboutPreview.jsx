d26a653d36e9ba5f85f670b5c3585c0910361e6c
Bruno Borghi <brunoignacioborghi1@gmail.com>
2026-05-20 17:00:49 -0300

    backend onlineeeeeee

--- src/components/AboutPreview.jsx
+++ src/components/AboutPreview.jsx
@@ -1,13 +1,9 @@
 import "../styles/about.css";
 
-import { Link } from "react-router-dom";
-
 import leaf from "../assets/decorations/leaf.png";
 
 function AboutPreview() {
-
   return (
-
     <section className="about_preview">
 
       <img
@@ -16,32 +12,26 @@ function AboutPreview() {
         className="leaf_decoration"
       />
 
-      <h2>
-        Sobre mí
-      </h2>
+      <h2>Sobre mí</h2>
 
       <p>
-
-        Soy Lucía Borghi, psicóloga clínica y sociocomunitaria dedicada a acompañar procesos emocionales individuales y grupales desde una mirada cercana, humana y profesional.
-
+        Soy Lucía Borghi, psicóloga dedicada a acompañar
+        procesos emocionales desde una mirada cercana,
+        humana y profesional.
       </p>
 
       <p>
-
-        Mi objetivo es brindarte un espacio seguro donde puedas comprender lo que te sucede y encontrar herramientas para sentirte mejor.
-
+        Mi objetivo es brindarte un espacio seguro donde
+        puedas comprender lo que te sucede y encontrar
+        herramientas para sentirte mejor.
       </p>
 
-      <Link to="/sobre-mi">
-
+      <a href="/sobre-mi">
         Leer más
-
-      </Link>
+      </a>
 
     </section>
-
   );
-
 }
 
 export default AboutPreview;
